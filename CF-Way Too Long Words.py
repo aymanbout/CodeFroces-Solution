480fc205a94bb6e21c907d6d803b8165a14fe561
aymanbout <ayman.boutadghart.yes@gmail.com>
2016-07-17 14:11:47 +0000

    Update and rename TooLong.py to CF-Way Too Long Words.py

---
+++ CF-Way Too Long Words.py
@@ -0,0 +1,13 @@
+words=[]
+n=eval(input())
+for i in range (n):
+  	n=input()
+  	words.append(n)
+  	if len(n)<=10:
+  		print (n)
+  	else:
+  		x=len(n)
+  		numofchar= x-2
+  		lastchar=n[-1]
+  		resulat=str(n[0])+str(numofchar)+str(lastchar)
+  		print(resulat,end="\n")
